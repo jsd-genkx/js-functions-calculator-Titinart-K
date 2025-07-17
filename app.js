eea7dab771a17dbbd05065e3f497132008e31825
titinart <k.titinart@gmail.com>
2025-07-17 10:26:11 +0700

    calculator solution

---
+++ app.js
@@ -0,0 +1,17 @@
+const nums = [1, 2, 3];
+const len = nums.length;
+console.log(len);
+
+nums.push(4);
+nums.pop();
+console.log(nums);
+
+nums.unshift();
+console.log(nums);
+
+nums.shift();
+console.log(nums);
+
+nums.splice(1,1,9);
+console.log(nums);
+
